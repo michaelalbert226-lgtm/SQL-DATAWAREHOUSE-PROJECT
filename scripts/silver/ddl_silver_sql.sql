0f4079b227be0ca12f4f5cfa2031a42a180a5ab0
Mikkymayor <michaelalbert226@gmail.com>
2026-03-19 11:37:38 +0100

    Create silver schema tables for customer and product data
    This script drops existing tables and creates new tables for customer information, product information, sales details, and various ERP-related data in the silver schema.

---
+++ scripts/silver/ddl_silver_sql.sql
@@ -0,0 +1,73 @@
+DROP TABLE IF EXISTS silver.crm_cust_info;      
+
+CREATE TABLE silver.crm_cust_info (
+    cst_id int,
+    cst_key nvarchar(50),
+    cst_firstname nvarchar(100),
+    cst_lastname nvarchar(100),
+    cst_material_status nvarchar(50),
+    cst_gndr nvarchar(10),
+    cst_create_date DATE,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+
+);
+
+-- Create the bronze layer table for customer product information if it exists drop it first
+DROP TABLE IF EXISTS silver.crm_prd_info;
+
+CREATE TABLE silver.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+-- Create the bronze layer table for customer sales details if it exists drop it first
+DROP TABLE IF EXISTS silver.crm_sales_details;
+
+CREATE TABLE silver.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+
+DROP TABLE IF EXISTS silver.erp_loc_a101;
+
+
+CREATE TABLE silver.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+
+);
+
+
+DROP TABLE IF EXISTS silver.erp_cust_az12;
+CREATE TABLE silver.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+
+DROP TABLE IF EXISTS silver.erp_px_cat_g1v2;
+CREATE TABLE silver.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
